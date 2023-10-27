c0a394d62a41d658004bbf56115092099efdc90a
Tatiana Novitskaya <passatik11@gmail.com>
2023-10-26 22:38:59 -0400

    created structure

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import MainSection from "@/components/sections/MainSection";
+
+export default function Home() {
+  return (
+    <main>
+      <MainSection/>
+    </main>
+  )
+}
