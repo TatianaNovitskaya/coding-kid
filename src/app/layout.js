b8c1f0d7df6fa0eb94e7e3a05cc5cded8b181e1d
Tatiana Novitskaya <passatik11@gmail.com>
2023-10-28 22:28:31 -0400

    added header

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import {Inter} from 'next/font/google'
 import './globals.css'
 import Header from "@/components/Header";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({subsets: ['latin'], weight:["400", "500", "700", "800"]})
 
 export const metadata = {
     title: 'Create Next App',
