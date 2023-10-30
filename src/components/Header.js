b56ca6c157bae79a2e698c13142a34dea27c95a2
Tatiana Novitskaya <passatik11@gmail.com>
2023-10-30 11:11:11 -0400

    changed text

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,22 @@ export default function Header() {
             <header className={"fixed top-0 left-0 right-0 backdrop-blur-sm z-50"}>
                 <div className={"container mx-auto flex justify-between items-center py-[2.62rem]"}>
                     <div className="flex items-center">
-                        <p className={"text-[#00235C] font-extrabold text-[1.41rem] bg-white leading-[1.10rem] tracking-[.028rem] p-[0.59rem]"}>KC</p>
-                        <span className={"text-white uppercase font-extrabold block text-[0.79rem] leading-[1.10rem] tracking-[.016rem] bg-[#00235C] p-[0.59rem]"}>kids coding</span>
+                        <p className={"text-[#00235C] font-extrabold text-[1.41rem] bg-white leading-[1.10rem] tracking-[.028rem] p-[0.59rem]"}>KCA</p>
+                        <span
+                            className={"text-white uppercase font-extrabold block text-[0.79rem] leading-[1.10rem] tracking-[.016rem] bg-[#00235C] p-[0.59rem]"}>Kids` Coding Adventure</span>
                     </div>
                     <nav>
                         <ul className={"flex text-white text-base font-medium"}>
                             <li><Link href={"#"} className={"mr-[5.75rem] hover:font-extrabold"}>Home</Link></li>
                             <li><Link href={"#"} className={"mr-[5.75rem] hover:font-extrabold"}>Class</Link></li>
-                            <li><Link href={"#"} className={"mr-[5.75rem] hover:font-extrabold"}>Our activity</Link></li>
+                            <li><Link href={"#"} className={"mr-[5.75rem] hover:font-extrabold"}>Our activity</Link>
+                            </li>
                             <li><Link href={"#"} className={"mr-[5.75rem] hover:font-extrabold"}>Contact us</Link></li>
                         </ul>
                     </nav>
-                    <button className={"py-[0.62rem] px-[1.88rem] bg-white text-[#116CFF] font-bold rounded-[0.625rem]"}>Login</button>
+                    <button
+                        className={"py-[0.62rem] px-[1.88rem] bg-white text-[#116CFF] font-bold rounded-[0.625rem]"}>Login
+                    </button>
                 </div>
             </header>
         </>
--- src/components/sections/MainSection.js
+++ src/components/sections/MainSection.js
@@ -7,10 +7,22 @@ export default function MainSection() {
             <section id={"home"} className={"bg-[#116CFF]"}>
                 <div className={"container mx-auto flex justify-between items-center py-[9.62rem]"}>
                     <div className={"w-1/2 text-white"}>
-                        <h1 className={" text-[3.125rem] leading-[5rem] font-extrabold"}>Learn Coding For Kids From Anywhere</h1>
-                        <p className={"mt-4 w-[70%] text-[1.125rem] leading-[2.025rem] font-medium"}>Discover the best coding course for your kids. Learn Coding from basic.</p>
-                        <button className={"mr-[1.5rem] mt-[2rem] py-[0.94rem] px-[2.31rem] bg-white text-[#116CFF] font-bold rounded-[0.625rem]"}>Book a Class</button>
-                        <Link href={"#"} className={"py-[0.94rem] px-[2.31rem] border border-white text-white font-medium rounded-[0.625rem]"}>Learn more</Link>
+                        <h1 className={" text-[3.125rem] leading-[5rem] font-extrabold"}>Welcome to Kids' Coding
+                            Adventure!</h1>
+                        <p className={"mt-4 text-[1.5rem] leading-[2.025rem] font-medium"}>Unlock the World of
+                            Creativity and Innovation through Coding</p>
+                        <p className={"mt-4 w-[85%] text-[1rem] leading-[1.75rem]"}>At Kids' Coding Adventure, we
+                            believe in empowering the next generation of innovators. Our fun and engaging coding
+                            programs are designed to introduce children to the exciting world of technology and
+                            programming. Join us on a journey of creativity, problem-solving, and endless
+                            possibilities!</p>
+                        <button
+                            className={"mr-[1.5rem] mt-[2rem] py-[0.94rem] px-[2.31rem] bg-white text-[#116CFF] font-bold rounded-[0.625rem]"}>Book
+                            a Class
+                        </button>
+                        <Link href={"#"}
+                              className={"py-[0.94rem] px-[2.31rem] border border-white text-white font-medium rounded-[0.625rem]"}>Learn
+                            more</Link>
                     </div>
                     <div className={"w-1/2"}>
                         <Image src={"/svg/online-learning.svg"} alt={"remote"} width={632} height={387}/>
