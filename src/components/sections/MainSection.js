7b56e159549d6c43bf57a406b24f10d4e5ab6732
Tatiana Novitskaya <passatik11@gmail.com>
2023-11-04 18:34:58 -0400

    fixed styles

--- src/components/sections/MainSection.js
+++ src/components/sections/MainSection.js
@@ -5,7 +5,7 @@ export default function MainSection() {
     return (
         <>
             <section id={"home"} className={"bg-[#116CFF]"}>
-                <div className={"container mx-auto px-4 flex flex-col-reverse lg:flex-row-reverse justify-between items-center py-[5.62rem] lg:py-[9.62rem]"}>
+                <div className={"container mx-auto px-4 flex flex-col-reverse lg:flex-row-reverse justify-between items-center py-[5.62rem] xl:py-[9.62rem]"}>
                     <div className={"w-full lg:w-1/2 text-white text-center lg:text-left"}>
                         <h1 className={"text-[2.725rem] lg:text-[3.125rem] leading-[3rem] lg:leading-[5rem] font-extrabold"}>Welcome to Kids&apos; Coding
                             Adventure!</h1>
@@ -16,12 +16,12 @@ export default function MainSection() {
                             programs are designed to introduce children to the exciting world of technology and
                             programming. Join us on a journey of creativity, problem-solving, and endless
                             possibilities!</p>
-                        <button
-                            className={"mr-[1rem] lg:mr-[1.5rem] mt-[2rem] py-[0.81rem] px-[1.88rem] lg:py-[0.94rem] lg:px-[2.31rem] bg-white text-[#116CFF] font-bold rounded-[0.625rem]"}>Book
+                        <Link href={"/#contacts"}
+                            className={"mr-[1rem] inline-block lg:mr-[1.5rem] mt-[2rem] py-[0.81rem] px-[1.88rem] lg:py-[0.94rem] lg:px-[2.31rem] bg-white text-[#116CFF] font-bold rounded-[0.625rem]"}>Book
                             a Class
-                        </button>
+                        </Link>
                         <Link href={"#programs"}
-                              className={"py-[0.81rem] px-[1.88rem] lg:py-[0.94rem] lg:px-[2.31rem] border border-white text-white font-medium rounded-[0.625rem]"}>Learn
+                              className={"py-[0.81rem] inline-block px-[1.88rem] lg:py-[0.94rem] lg:px-[2.31rem] border border-white text-white font-medium rounded-[0.625rem]"}>Learn
                             more</Link>
                     </div>
                     <div className={"w-full mb-[5rem] lg:mb-0 lg:w-1/2"}>
